d9506b786e784d423dfd39391d2c1dc072e8135e
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2023-01-27 09:53:06 +0700

    Set Authentication

---
+++ OrderingApp/OrderingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
